a75068575f0fd31752aa1ab5553644d028c5e1a6
Strryke <gavinong2000@gmail.com>
2022-04-05 14:43:23 +0800

    added moderation service

--- comments/index.js
+++ comments/index.js
@@ -19,7 +19,7 @@ app.post("/posts/:id/comments", async (req, res) => {
 
   const comments = commentsByPostId[req.params.id] || [];
 
-  comments.push({ id: commentId, content });
+  comments.push({ id: commentId, content, status: "pending" });
   commentsByPostId[req.params.id] = comments;
   await axios.post("http://localhost:4005/events", {
     type: "CommentCreated",
@@ -27,13 +27,39 @@ app.post("/posts/:id/comments", async (req, res) => {
       id: commentId,
       content,
       postId: req.params.id,
+      status: "pending",
     },
   });
   res.status(201).send(comments);
 });
 
-app.post("/events", (req, res) => {
+app.post("/events", async (req, res) => {
   console.log("event received", req.body.type);
+  const { type, data } = req.body;
+
+  if (type == "CommentModerated") {
+    // console.log(data);
+    const { postId, id, status, content } = data;
+    const comments = commentsByPostId[postId];
+
+    const comment = comments.find((comment) => {
+      return comment.id === id;
+    });
+
+    comment.status = status;
+
+    await axios
+      .post("http://localhost:4005/events", {
+        type: "CommentUpdated",
+        data: {
+          id,
+          status,
+          postId,
+          content,
+        },
+      })
+      .catch((err) => console.log(err.message));
+  }
   res.send({});
 });
 
